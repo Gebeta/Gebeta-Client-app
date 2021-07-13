6e4d25cb3ff58e2f59e7d000f1783f57149f0884
Yael <eyaelabdissa8@gmail.com>
2021-07-13 20:55:16 +0300

    color constant added

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const gPrimaryColor = Color(0xffcb4d2b);
+const gsecondaryColor  =Color(0xffD86346);
+const ksecondaryColor = Color(0xFFB5BFD0);
+const gTextColor = Color(0xFF50505D);
+const gTextLightColor = Color(0xFF6A727D);
+const whiteColor = Colors.white;
