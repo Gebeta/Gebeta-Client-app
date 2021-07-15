56d9aa71eef2bf085917f4473af11383a1f73560
Yael <eyaelabdissa8@gmail.com>
2021-07-15 11:44:20 +0300

    fontfamily  changed

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // brightness: Brightness.dark,
         primaryColor: gsecondaryColor,
-        fontFamily: "Raleway"
+        fontFamily: "Montserrat"
       ),
       home: HomeScreen(),
       // home: MyHomePage(),
