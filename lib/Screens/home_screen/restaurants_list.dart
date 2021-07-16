5447ea7a2642998f19bd1c2a06303204f776e12a
Yael <eyaelabdissa8@gmail.com>
2021-07-16 09:20:59 +0300

    restaurant list modified

--- lib/Screens/home_screen/restaurants_list.dart
+++ lib/Screens/home_screen/restaurants_list.dart
@@ -10,81 +10,80 @@ class RestaurantsList extends StatefulWidget {
 }
 
 Widget _listItem(
-    String imgPath, String foodName, String desc, double rating, bool isOpen) {
+    String imgPath, String restaurantName, String desc, double rating, bool isOpen) {
   return Padding(
     padding: const EdgeInsets.all(10.0),
-    child: Stack(
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.0),
-            color: Colors.white,
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              imgPath,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
           ),
-          child: Column(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  imgPath,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        foodName,
-                        style: TextStyle(
-                            color: Color(0xFF563734),
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 8, right: 5),
-                        padding: EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(
-                            color: isOpen ? Color(0xff0267C1) : Colors.red,
-                            borderRadius: BorderRadius.circular(4)),
-                        child: Text(
-                          isOpen ? "Open" : "Closed",
-                          style: TextStyle(color: whiteColor),
-                        ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    child: RatingBarIndicator(
-                      rating: rating,
-                      itemBuilder: (context, index) => Icon(
-                        Icons.star,
-                        color: gPrimaryColor,
-                      ),
-                      itemCount: 5,
-                      itemSize: 20.0,
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  Container(
-                    child: Text(
-                      desc,
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      restaurantName,
                       style: TextStyle(
-                          color: Color(0xFFB2A9A9),
+                          color: Color(0xFF563734),
                           fontFamily: 'Montserrat',
-                          fontSize: 15.0),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0),
                     ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8,),
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                          color: isOpen ? Color(0xff0267C1).withOpacity(0.65) : Colors.red.withOpacity(0.65),
+                          borderRadius: BorderRadius.circular(4)),
+                      child: Text(
+                        isOpen ? "Open" : "Closed",
+                        style: TextStyle(color: whiteColor),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  child: RatingBarIndicator(
+                    rating: rating,
+                    itemBuilder: (context, index) => Icon(
+                      Icons.star,
+                      color: gPrimaryColor,
+                    ),
+                    itemCount: 5,
+                    itemSize: 20.0,
                   ),
-                  SizedBox(height: 10.0),
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+                ),
+                SizedBox(height: 10.0),
+                Container(
+                  child: Text(
+                    desc,
+                    style: TextStyle(
+                        color: Color(0xFFB2A9A9),
+                        fontFamily: 'Montserrat',
+                        fontSize: 15.0),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+              ],
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
