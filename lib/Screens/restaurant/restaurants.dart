8fc562086a863f284eba495373199dfb9936de47
Yael <eyaelabdissa8@gmail.com>
2021-07-18 08:36:03 +0300

    all restaurants page added

---
+++ lib/Screens/restaurant/restaurants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gebeta_food/Screens/home_screen/restaurants_list.dart';
+
+import '../../constants.dart';
+
+class AllRestaurantsPage extends StatelessWidget {
+  const AllRestaurantsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // automaticallyImplyLeading: false,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios)),
+          title: Text(
+            "Restaurants",
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+        body: ListView(
+          children: [
+            RestaurantsList(),
+            RestaurantsList()
+            
+          ],
+        ));
+  }
+}
