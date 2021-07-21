f79155b0f2581c67cbd5187370eaf6cad5db5008
Yael <eyaelabdissa8@gmail.com>
2021-07-21 10:23:20 +0300

    user model added

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+class User {
+  late final String id;
+  late final String email;
+  late final String password;
+
+  User({required this.id, required this.email, required this.password});
+}
---
+++ lib/scoped-models/user.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
+
+class UserModel extends Model {
+  Future<Map<String, dynamic>> signUp(
+      String id,
+      String fname,
+      String lname,
+      String username,
+      String password,
+      String email,
+      String phoneNo,
+      String address) async {
+    final Map<String, dynamic> userData = {
+      "id": id,
+      "first_name": fname,
+      "last_name": lname,
+      "username": username,
+      "password": password,
+      "email": email,
+      "phone_no": phoneNo,
+      "address": address
+    };
+    Uri url = Uri.parse("http://10.0.2.2:3000/client");
+    final http.Response response = await http.post(url,
+        body: json.encode(userData),
+        headers: {'Content-Type': 'application/json'});
+
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    print(responseData);
+
+    return {'success': true, 'message': "signed up"};
+  }
+}
